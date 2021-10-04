7327642cc5fb082f65cd47db401dea334d4eb8cc
Thobif <jirayootpandee537@gmail.com>
2021-10-04 16:25:33 +0700

    update

---
+++ js/prommise.js
@@ -0,0 +1,11 @@
+let counValue = new Promise((resolve, reject )=>{
+    if (true){
+        resolve("There is a count value");
+
+    }
+    else {
+        reject("There is no count valure");
+    }
+    
+})
+console.log(counValue);
